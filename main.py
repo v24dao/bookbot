01558c2edcf2d592bf6d426daa99f2e219bcabb1
v24dao <v24dao@gmail.com>
2025-09-21 18:49:50 +0100

    Add functionality to print report

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from stats import get_num_words, get_char_counts
+from stats import get_num_words, get_char_counts, char_dict_to_sorted_list
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    print(get_char_counts(text))
+    char_counts = get_char_counts(text)
+    sorted_list = char_dict_to_sorted_list(char_counts)
+    print_report(book_path, num_words, sorted_list)
     
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-    
+
+def print_report(book_path, num_words, sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for x in sorted_list:
+        if x["char"].isalpha():
+            print(f"{x['char']}: {x['num']}")
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -11,4 +11,12 @@ def get_char_counts(text):
         counts[lowered] += 1
     return dict(counts)
 
+def char_dict_to_sorted_list(dict):
+    sorted_list = []
+    for char,num in dict.items():
+        sorted_list.append({"char":char ,"num": num})
+    sorted_list.sort(reverse = True, key = lambda dict: dict["num"])
+    return sorted_list
 
+
+    
