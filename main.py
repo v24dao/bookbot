0dc9c6117db5117263f76d9c2228169416fa631b
v24dao <v24dao@gmail.com>
2025-09-21 01:27:20 +0100

    refactor to separate get_num_words

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_word_count(text)
+    num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
     
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-
-
-def get_word_count(text):
-    words = text.split()
-    return len(words)
-
+    
 main()
