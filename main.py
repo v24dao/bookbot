a86c6a9924dc904f6e8ec9bd27f258ad77ed5f76
v24dao <v24dao@gmail.com>
2025-09-21 01:57:00 +0100

    Add get_char_couts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_counts
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(get_char_counts(text))
     
 def get_book_text(file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_counts(text):
+    counts = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered not in counts:
+            counts[lowered] = 0
+        counts[lowered] += 1
+    return counts
